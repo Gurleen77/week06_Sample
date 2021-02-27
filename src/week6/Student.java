d5224a4652310b7abeec46743277a875b1e05652
Navdeep92 <balgurleen77@gmail.com>
2021-02-27 20:19:11 +0530

    Initial commit

--- src/week6/Student.java
+++ src/week6/Student.java
@@ -1,4 +1,23 @@
 package week6;
 
+import sun.util.calendar.BaseCalendar;
+
+import java.util.Date;
+
 public class Student {
+    private int student_id;
+    private String  first_name;
+    private String last_name;
+    private Date birth_date;
+    private Enum gender; //Enum
+    private String address;
+    private String  city;
+    private String cell_phone;
+    private String email;
+    private String department;
+    private float  marks[];
+    private float total_marks;
+    private float percentage;
+    private String grade; //A, A+
+
 }
--- src/week6/classActivity.java
+++ src/week6/classActivity.java
@@ -1,9 +1,5 @@
 package week6;
 
 public class classActivity {
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 5494184 (Initial commit)
 }
