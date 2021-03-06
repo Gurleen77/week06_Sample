73ae092667b5fb22891411a5c44bd98aef69dd8a
Navdeep92 <balgurleen77@gmail.com>
2021-03-06 22:15:01 +0530

    percentage & grade

---
+++ src/week6/Sample.java
@@ -0,0 +1,50 @@
+package week6;
+
+
+import java.util.Date;
+
+public class Sample {
+    public static void main(String[] args) {
+        /*Student s1 = new Student(1);
+        s1.setFirstName("Pritesh");
+        s1.setLastName("Patel");*/
+
+        float[] dalveerMarks = {80, 80, 80, 80, 80};
+        Student dalveer = new Student(789821,
+                "Dalveer",
+                "Singh",
+                new Date(),
+                Gender.Male,
+                "String address",
+                "String city",
+                "String pinCode",
+                "String cellPhone",
+                "String email",
+                "String department",
+                dalveerMarks);
+        dalveer.calculateTotalMarks();
+        dalveer.calculatePercentage();
+        dalveer.calculateGrade();
+
+
+        float[] dwarkeshMarks = {90, 90, 90, 90, 90};
+        Student dwarkesh = new Student(789822,
+                "Dwarkesh",
+                "Singh",
+                new Date(),
+                Gender.Male,
+                "String address",
+                "String city",
+                "String pinCode",
+                "String cellPhone",
+                "String email",
+                "String department",
+                dwarkeshMarks);
+        dwarkesh.calculateTotalMarks();
+        dwarkesh.calculatePercentage();
+        dwarkesh.calculateGrade();
+
+        System.out.println("Dalveer Grades: " + dalveer.getGrade());
+        System.out.println("Dwarkesh Grades: " + dwarkesh.getGrade());
+    }
+}
